5a144051a0b2190fc68d31e0f1251eb811ddd01b
sorobsocb <105376063+sorobsocb@users.noreply.github.com>
2022-05-11 17:34:14 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "Orange_Business_Services"
     workspaces {
       name = "ace-iac-day-zero"
     }
