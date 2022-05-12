e3fcae454f136266b8e925c1f59538618f344f0a
Sevan ORANCI <105376063+sorobsocb@users.noreply.github.com>
2022-05-12 14:02:32 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,6 @@
 terraform {
-  required_version = ">= 1.1.0"
+  #required_version = ">= 1.1.0"
+    required_version = "1.0.0"
   backend "remote" {
     hostname = "app.terraform.io"
     organization = "Orange_Business_Services"
