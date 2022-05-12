b45ce3f5ce21e295aaa2a7d5c9843fc00c707373
Sevan ORANCI <105376063+sorobsocb@users.noreply.github.com>
2022-05-12 11:51:48 +0200

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -12,7 +12,7 @@ data "aws_ami" "ubuntu" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["560101643459"] # Canonical
+  owners = ["099720109477"] # Canonical
 }
 
 locals {
